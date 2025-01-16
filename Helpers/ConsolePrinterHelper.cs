9d492b352373995351f1d753ad674cbab596a3f0
Your Name <jacobodb21@gmail.com>
2025-01-16 17:04:55 -0600

    created helpers classes to print on console and some team related methods

---
+++ Helpers/ConsolePrinterHelper.cs
@@ -0,0 +1,40 @@
+﻿namespace Standings.Helpers
+{
+    public static class ConsolePrinterHelper
+    {
+
+        public static void startMessage()
+        {
+            Console.WriteLine("---- Team Standings ----");
+        }
+
+        public static void instructionsMessage()
+        {
+            Console.WriteLine("Please select the origin of your results:");
+            Console.WriteLine("1) From a file");
+            Console.WriteLine("2) Console interface");
+            Console.WriteLine("Type your selection: ");
+        }
+
+        public static void exitMessage()
+        {
+            Console.WriteLine("---- Bye ----");
+        }
+
+        public static void selectionMessage()
+        {
+            Console.WriteLine("Type your path to the file: ");
+        }
+
+        public static void standingsTable(Dictionary<string, int> standings)
+        {
+            int position = 1;
+            Console.WriteLine("== STANDINGS ==");
+            foreach (var team in standings)
+            {
+                Console.WriteLine(position + ". " + team.Key + ", " + team.Value + " pts");
+                position++;
+            }
+        }
+    }
+}
---
+++ Helpers/TeamHelper.cs
@@ -0,0 +1,38 @@
+﻿namespace Standings.Helpers
+{
+    public static class TeamHelper
+    {
+
+        public static string formTeamName(string[] teamName)
+        {
+            string teamNameResult = "";
+            for (int i = 0; i < teamName.Length - 1; i++)
+            {
+                teamNameResult += " " + teamName[i];
+            }
+            return teamNameResult.Trim();
+        }
+
+        public static int getScoreFromTeam(string[] teamScore)
+        {
+            return int.Parse(teamScore[teamScore.Length - 1]);
+        }
+
+        public static int calculatePointsForTheTeam(int team1Score, int team2Score)
+        {
+            if (team1Score > team2Score)
+            {
+                return 3;
+            }
+            else if (team2Score > team1Score)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+
+        }
+    }
+}
